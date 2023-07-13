524014b123889cdcaf979d442e0feb1dffce5dac
Sharan Adhitya <94825435+TpNuke@users.noreply.github.com>
2023-07-13 11:59:33 +0530

    Create diagonal string.c

---
+++ lab 06/diagonal string.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char c[100];
+    scanf("%s",c);
+    int n=strlen(c);
+    int p=1;
+    for(int i=0;i<n;i++){
+        for(int j=0;n%2==0 ? j<n/2 : j<=n/2;j++){
+           
+        }
+        if(i<=n/2)
+            printf("%*s",i,"");
+        if(i>n/2)
+            printf("%*s",n-1-i,"");
+        if(i<=n/2)    
+            printf("%c",c[i]);    
+        else
+            printf("%c",c[n-1-i]);
+        if(i<n/2)
+            printf("%*s",(n-2)-2*i,"");
+        else if(i>(n/2)){
+            printf("%*s",(2*p)-1,"");
+            p++;
+        }
+        if(i<n/2)
+            printf("%c",c[n-1-i]);
+        else if(i>(n/2)){
+            printf("%c",c[i]);
+        }
+        printf("\n");
+        
+    }
+    return 0;
+}
